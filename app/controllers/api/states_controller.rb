58635cd7b20666bee5e138ff66b00422735a7480
James Boyette <james.boyette@gmail.com>
2021-06-07 19:41:50 -0400

    Add Index and Show methods to States controller, Update Index and Show view files

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,6 +1,6 @@
 class Api::StatesController < ApplicationController
   def index
-
+    @states = State.all
     # render json: "All the states' horses and all the states' men ..."
     # @alignments = Alignment.all
     render "index.json.jb"
@@ -9,15 +9,20 @@ class Api::StatesController < ApplicationController
   def show
 
     # @state = State.find_by(abbrev: params[:abbrev])
-    p "HERE ARE THE PARAMS"
-    p params
-    # @state = Abbreviation.find_by(state: params[:code])
-    p "HERE'S THE STATE INFO VIA HARDCODING"
-    @state = Abbreviation.find_by(code: "FL")
-    p @state
-    p "HERE'S THE STATE INFO VIA PARAMS"
-    @state = Abbreviation.find_by(code: params[:code])
-    p @state
+    # p "HERE ARE THE PARAMS"
+    # p params
+    # p "HERE'S THE STATE CODE INFO VIA HARDCODING"
+    # @state_code = Abbreviation.find_by(code: "FL")
+    # p @state_code
+    # p "HERE'S THE STATE CODE INFO VIA PARAMS"
+    @state_code = Abbreviation.find_by(code: params[:code])
+    # p @state_code
+    # p "Here's our state name : "
+    state = @state_code.state
+    # p state
+    # p "HERE'S THE STATE INFO VIA PARAMS"
+    @state = State.find_by(state: state)
+    # p @state
     render "show.json.jb"
   end
 end
